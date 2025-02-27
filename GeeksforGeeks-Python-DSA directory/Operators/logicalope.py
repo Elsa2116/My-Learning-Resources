c3f7f7f9414143ff708e1437ac8ea69b3ceeb265
Elsa2116 <elsialemayehu33@gmail.com>
2025-02-27 13:16:16 -0500

    Add files via upload

---
+++ GeeksforGeeks-Python-DSA directory/Operators/logicalope.py
@@ -0,0 +1,12 @@
+age = 25
+has_license = True
+if age >= 18 and has_license:
+            print("Eligible to drive")
+else:
+            print("Not eligible to drive")
+is_weekend = False
+is_raining = True
+if is_weekend or not is_raining:
+            print("Good day!")
+else:
+            print("Bad day!")   
